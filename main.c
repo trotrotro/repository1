2b8825509040a878e78bb2a5cfaeb552fe1050ac
trotrotro <70337484+trotrotro@users.noreply.github.com>
2020-08-27 22:55:22 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,6 +2,19 @@
 #include <math.h>
 
 #define ALEF 1948
+#define TEST
+
+/**
+    \brief solves a square equation ax^2 + bx + c = 0
+
+    \param a first  coefficient
+    \param b second coefficient
+    \param c third  coefficient
+    \param x1 the only root or one of roots
+    \param x2 another root
+
+    \return amount of roots or const ALEF, when amount is infinite
+*/
 
 int solution(double a, double b,
      double c, double *x1, double *x2){
@@ -31,7 +44,42 @@ int solution(double a, double b,
     return 2;
 }
 
+void sum_of_tests(){
+    double x1 = 0, x2 = 0;
+    //1
+    if (solution(0, 0, 0, &x1, &x2) == ALEF)
+        printf("test 1 is passed\n");
+    else
+        printf("test 1 is failed\n");//all zeros
+    //2
+    if (solution(1, 0, -4, &x1, &x2) == 2 && x1*x2 == -4 && x1+x2 == 0)
+        printf("test 2 is passed\n");
+    else
+        printf("test 2 is failed\n");//correct equation
+    //3
+    if (solution(0, 2, -4, &x1, &x2) == 1 && x1 == 2)
+        printf("test 3 is passed\n");
+    else
+        printf("test 3 is failed\n");//linear equation
+    //4
+    if (solution(1, 1, 1, &x1, &x2) == 0)
+        printf("test 4 is passed\n");
+    else
+        printf("test 4 is failed\n");//no roots
+    //5
+    if (solution(1, 2, 1, &x1, &x2) == 1 && x1 == -1)
+        printf("test 5 is passed\n");
+    else
+        printf("test 5 is failed\n");//only root
+}
+
 int main(){
+
+    #ifdef TEST
+        sum_of_tests();
+        return 0;
+    #endif
+
     printf("enter a, b, c:\n");
     double a = 0, b = 0, c = 0;
 
